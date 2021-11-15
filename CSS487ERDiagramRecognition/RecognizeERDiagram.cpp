182114674e2fd69e53651179f9b7461517643294
allangg107 <allangenarigaarden@hotmail.com>
2021-11-14 18:22:04 -0800

    basic contouring

--- CSS487ERDiagramRecognition/RecognizeERDiagram.cpp
+++ CSS487ERDiagramRecognition/RecognizeERDiagram.cpp
@@ -2,4 +2,28 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+using namespace std;
 using namespace cv;
+
+int main()
+{
+	Mat testImage = imread("rectangle.png");
+
+	Mat testImageGray;
+	cvtColor(testImage, testImageGray, COLOR_BGR2GRAY);
+
+	Mat thresh;
+	threshold(testImageGray, thresh, 150, 255, THRESH_BINARY);
+	imshow("Binary mage", thresh);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	
+	Mat testImageCopy = testImage.clone();
+	findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+	drawContours(testImageCopy, contours, -1, Scalar(120, 0, 120), 2);
+	imshow("None approximation", testImageCopy);
+
+	waitKey(0);
+	destroyAllWindows();
+}
